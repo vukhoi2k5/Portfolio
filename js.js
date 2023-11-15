3a75293eaf0d495b39ef1736805da5061cecde48
vukhoi2k5 <vukhoi353@gmail.com>
2023-11-15 20:13:00 +0700

    ihvfuha

---
+++ js.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.my-div a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
